804dce332c49a47c1f7e0a5081b56c743076b867
Diksha Gupta <dikshaguptax86@gmail.com>
2023-09-03 20:22:56 +0530

    D67|(62. Unique Paths)

---
+++ Day67.py
@@ -0,0 +1,20 @@
+class Solution:
+    def uniquePaths(self, m: int, n: int) -> int:
+        
+        dp=[[0]*n for _ in range(m)]
+        
+        j=0
+        for i in range(m):
+            dp[i][j]=1
+        i=0
+        for j in range(1, n):
+            dp[i][j]=1
+        
+        for i in range(1, m):
+            for j in range(1, n):
+                
+                dp[i][j]=dp[i-1][j]+dp[i][j-1]
+                
+        
+        return dp[m-1][n-1]
+        
